91dafb3dc6ec8a7ae573e5e65c2c261785f0a668
Sandro Cunha <sandromedeirosdacunha@gmail.com>
2023-07-18 17:33:24 -0300

    disabel spring security

--- src/main/java/br/com/trier/springvespertino/resources/RunwayResource.java
+++ src/main/java/br/com/trier/springvespertino/resources/RunwayResource.java
@@ -27,26 +27,26 @@ public class RunwayResource {
 	@Autowired
 	private CountryService countryService;
 	
-	@Secured({"ROLE_USER"})
+//	@Secured({"ROLE_USER"})
 	@GetMapping("/{id}")
 	public ResponseEntity<Runway> findById(@PathVariable Integer id){
 		return ResponseEntity.ok(service.findById(id));
 	}
 	
-	@Secured({"ROLE_ADMIN"})
+//	@Secured({"ROLE_ADMIN"})
 	@PostMapping
 	public ResponseEntity<Runway> insert(@RequestBody Runway runway){
 		countryService.findById(runway.getCountry().getId());
 		return ResponseEntity.ok(service.insert(runway));
 	}
 	
-	@Secured({"ROLE_USER"})
+//	@Secured({"ROLE_USER"})
 	@GetMapping
 	public ResponseEntity<List<Runway>> listAll(){
 		return ResponseEntity.ok(service.listAll());
 	}
 	
-	@Secured({"ROLE_ADMIN"})
+//	@Secured({"ROLE_ADMIN"})
 	@PostMapping("/{id}")
 	public ResponseEntity<Runway> update (@RequestBody Runway runway, @PathVariable Integer id){
 		countryService.findById(runway.getId());
@@ -55,27 +55,27 @@ public class RunwayResource {
 		
 	}
 	
-	@Secured({"ROLE_ADMIN"})
+//	@Secured({"ROLE_ADMIN"})
 	@DeleteMapping
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.ok().build();
 	}
 	
-	@Secured({"ROLE_USER"})
+//	@Secured({"ROLE_USER"})
 	@GetMapping("/name/{name}")
 	public ResponseEntity<List<Runway>> findByNameStartsWithIgnoreCase(@PathVariable String name){
 		return ResponseEntity.ok(service.findByNameStartsWithIgnoreCase(name));
 		
 	}
 	
-	@Secured({"ROLE_USER"})
+//	@Secured({"ROLE_USER"})
 	@GetMapping("/size/{sizeIn}/{sizeFin}")
 	public ResponseEntity<List<Runway>> findBySizeBetween(@PathVariable Integer sizeIn, @PathVariable Integer sizeFin){
 		return ResponseEntity.ok(service.findBySizeBetween(sizeIn, sizeFin));
 	}
 	
-	@Secured({"ROLE_USER"})
+//	@Secured({"ROLE_USER"})
 	@GetMapping("/country/{idPais}")
 	public ResponseEntity<List<Runway>> findByCountryOrderBySizeDesc(@PathVariable Integer idPais){
 		return ResponseEntity.ok(service.findByCountryOrderBySizeDesc(countryService.findById(idPais)));
